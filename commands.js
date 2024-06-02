9e10b83d4579f7d412940bd79556805684b949af
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2024-06-02 00:53:19 -0500

    committing to submit after reviewing"

--- commands.js
+++ commands.js
@@ -91,14 +91,15 @@ db.bounties.find( { location: "Grasslands" })
 // 2. Query for all bounties with a reward worth 10000 or more
 db.bounties.find({ reward: { $gte: 10000 } })
 // 3. Query for all bounties, but exclude the client attribute from being shown
-
+db.bounties.find{}, {client: 0}
 // 4. Query for a Groundhog in the Woodlands
-
+db.bounties.find{species: "Groundhog", localation: "Woodland"}
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
-
+db.bounties.updateOne({name: "Polarwind", {$set: {reward: 1000}}})
 // 2. Remove Lokinkajou
-
+db.bounties.deleteOne({name: "Lokinkajou"})
 // 3. Delete all bounties sent by Songbird
-
+db.bounties.deleteMany({client: "Songbird"})
 // 4. Update all captured statuses to true
+db.bounties.updateMany({}, { $set: {captured: true}})
